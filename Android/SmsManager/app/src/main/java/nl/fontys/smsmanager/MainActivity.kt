6ae15ca8e7398d8003d4c07365bd5a05962b13bd
I407846 <o.aboudehn@student.fontys.nl>
2022-09-06 22:44:14 +0200

    sms app

---
+++ Android/SmsManager/app/src/main/java/nl/fontys/smsmanager/MainActivity.kt
@@ -0,0 +1,64 @@
+package nl.fontys.smsmanager
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.telephony.SmsManager
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.jar.Manifest
+
+
+class MainActivity : AppCompatActivity() {
+
+    val SEND_SMS_PERMISSION_CODE : Int = 1
+
+    lateinit var number : EditText
+    lateinit var textMessage : EditText
+    lateinit var button : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        number = findViewById(R.id.editTextPhone)
+        textMessage = findViewById(R.id.editTextTextMultiLine)
+        button = findViewById(R.id.btnSend)
+
+//        button.isEnabled = false
+//
+//        if (checkPermission(android.Manifest.permission.SEND_SMS)) {
+//            button.isEnabled = true
+//        } else {
+//            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.SEND_SMS), SEND_SMS_PERMISSION_CODE)
+//        }
+    }
+
+
+    fun onSend(v: View) : Void?{
+        val phoneNumber : String = number.text.toString()
+        val txtMessage : String = textMessage.text.toString()
+
+        if (phoneNumber == null || phoneNumber.length == 0 || txtMessage == null || txtMessage.length == 0) {
+            return null;
+        }
+
+        if (checkPermission(android.Manifest.permission.SEND_SMS)) run {
+            var smsManager: SmsManager = SmsManager.getDefault()
+            smsManager.sendTextMessage(phoneNumber, null, txtMessage, null, null)
+            Toast.makeText(this, "Message Sent!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+        }
+        return null
+    }
+
+    private fun checkPermission(permission: String) : Boolean {
+        var check : Int = ContextCompat.checkSelfPermission(this, permission)
+        return check == PackageManager.PERMISSION_GRANTED
+    }
+}
