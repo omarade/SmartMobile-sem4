08ee9bac585820af38d7c6c63fd83d08abc57d97
I407846 <o.aboudehn@student.fontys.nl>
2022-09-09 20:25:35 +0200

    added a button and text view that display the number of clicks

--- Android/MyFirstApp/app/src/main/java/nl/fontys/myfirstapp/MainActivity.kt
+++ Android/MyFirstApp/app/src/main/java/nl/fontys/myfirstapp/MainActivity.kt
@@ -2,10 +2,23 @@ package nl.fontys.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.btn)
+        val tvCounter = findViewById<TextView>(R.id.tvCounter)
+        var timesClicked = 0
+
+        btnClickMe.setOnClickListener {
+            timesClicked++
+            tvCounter.text = "Times Clicked: " + timesClicked
+            Toast.makeText(this, "Button clicked", Toast.LENGTH_LONG).show()
+        }
     }
 }
